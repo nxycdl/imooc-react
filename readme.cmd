aafbc32c36ef9fea0026112e3c2bb2348cf4f93a
nxycdl <nxycdl@163.com>
2016-05-23 01:14:38 +0800

    init project

---
+++ readme.cmd
@@ -0,0 +1,34 @@
+React 工程的建立
+yeoman web工程腳手架
+
+npm install -g yo
+yo --version
+
+到yeoman 的官网 找 生成器； 搜索 react
+查看版本;
+npm ls -g --depth=1
+
+工程建立
+yo  yeoman
+react-webpack 生成器
+imooc-react   工程名
+yo react-webpack imooc-react
+
+
+目录说明
+node-modules  nojs依赖模块
+test          测试代码
+src           源代码
+.editconfig   代码风格配置
+eclintrc      代码分格检测工具
+yo-rc         yeoman的配置文件
+karma.conf.js kama测试框架配置文件
+package.json  node配置文件
+webpack.config.js webpack文件;
+
+
+运行
+node server.js
+
+
+chrome 
